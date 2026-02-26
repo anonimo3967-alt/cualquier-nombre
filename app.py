b35ba05898d17b9e53a9b041c64f16c3d7cfbe49
anonimo3967-alt <anonimo3967@gmail.com>
2026-02-26 10:15:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.title("Mi primera App!!")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
 st.write("Facilmente puedo realizar backend y frontend")
-image = Image.open('tequila_sunset.jpeg')
+image = Image.open('Tequila.jpeg')
 st.image(image, caption='Interfaces multimodales')
